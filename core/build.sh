f0480b47bfae9efdf7f96de6773705523dad851a
Ashutosh <mrashutheboy@gmail.com>
2026-03-12 17:37:46 +0530

    Fix Render build paths

--- core/build.sh
+++ core/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+SCRIPT_DIR="$(cd -- "$(dirname -- "$0")" && pwd)"
+MANAGE_PY="$SCRIPT_DIR/manage.py"
+
 python -m pip install --upgrade pip
-python manage.py collectstatic --no-input
-python manage.py migrate
+python "$MANAGE_PY" collectstatic --no-input
+python "$MANAGE_PY" migrate
